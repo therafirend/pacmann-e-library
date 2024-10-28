9dce35fa9a4d9b14fef6a65f4015594ea4597b19
rafi <rafiansyah@bangunindo.com>
2024-10-28 08:19:11 +0700

    adjust

--- test/test.py
+++ test/test.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import TimeoutException
 import time
 
 class TestMusicPlayer(unittest.TestCase):
@@ -19,9 +20,10 @@ class TestMusicPlayer(unittest.TestCase):
         self.driver = webdriver.Chrome(service=service, options=chrome_options)
         
         self.driver.get("http://localhost:5000")
-        self.driver.implicitly_wait(10)
+        self.driver.implicitly_wait(10)  # Reduced implicit wait
 
     def scroll_and_click(self, selector, wait_time=10):
+        """Utility method to scroll to an element and click it."""
         element = WebDriverWait(self.driver, wait_time).until(
             EC.presence_of_element_located((By.XPATH, selector))
         )
@@ -30,27 +32,39 @@ class TestMusicPlayer(unittest.TestCase):
         element.click()
 
     def js_click(self, selector, wait_time=10):
+        """Utility method to wait for an element and click it using JavaScript."""
         element = WebDriverWait(self.driver, wait_time).until(
             EC.presence_of_element_located((By.XPATH, selector))
         )
         self.driver.execute_script("arguments[0].click();", element)
 
     def wait_for_obstruction_to_hide(self, obstruction_selector, wait_time=10):
-        WebDriverWait(self.driver, wait_time).until(
-            EC.invisibility_of_element_located((By.XPATH, obstruction_selector))
-        )
+        """Utility method to wait for an obstruction to become invisible."""
+        try:
+            # Wait for the obstruction to be present first
+            WebDriverWait(self.driver, wait_time).until(
+                EC.presence_of_element_located((By.XPATH, obstruction_selector))
+            )
+            # Now wait for it to become invisible
+            WebDriverWait(self.driver, wait_time).until(
+                EC.invisibility_of_element_located((By.XPATH, obstruction_selector))
+            )
+            print("Obstruction is no longer visible.")
+        except TimeoutException:
+            print("Obstruction was still visible or not found.")
+            self.driver.get_screenshot_as_file("timeout_error.png")  # Take a screenshot for debugging
 
     def test_play_song(self):
-        self.wait_for_obstruction_to_hide("//div[@class='now-playing-bar']")
+        self.wait_for_obstruction_to_hide("//div[@class='now-playing-bar']", wait_time=15)
         self.scroll_and_click("//button[@onclick=\"playAudio(this.parentElement.getAttribute('data-url'), this.parentElement)\"]")
 
     def test_play_pause(self):
-        self.wait_for_obstruction_to_hide("//div[@class='now-playing-bar']")
+        self.wait_for_obstruction_to_hide("//div[@class='now-playing-bar']", wait_time=15)
         self.scroll_and_click("//button[@onclick='togglePlayPause()']")
         self.scroll_and_click("//button[@onclick='togglePlayPause()']")
 
     def test_next_previous_song(self):
-        self.wait_for_obstruction_to_hide("//div[@class='now-playing-bar']")
+        self.wait_for_obstruction_to_hide("//div[@class='now-playing-bar']", wait_time=15)
         self.scroll_and_click("//button[@onclick='nextSong()']")
         self.scroll_and_click("//button[@onclick='nextSong()']")
         self.scroll_and_click("//button[@onclick='previousSong()']")
@@ -59,6 +73,5 @@ class TestMusicPlayer(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
